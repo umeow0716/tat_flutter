5cef8761e04ec4377d54fca596812b126a6cacf4
jj30462281 <jj30462281@gmail.com>
2024-02-20 11:56:17 +0800

    delete useless class file

--- lib/src/model/course/course_json.dart
+++ lib/src/model/course/course_json.dart
@@ -4,6 +4,12 @@ import 'package:json_annotation/json_annotation.dart';
 
 part 'course_json.g.dart';
 
+// ignore: constant_identifier_names
+enum Day { Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday, UnKnown }
+
+// ignore: constant_identifier_names
+enum SectionNumber { T_1, T_2, T_3, T_4, T_N, T_5, T_6, T_7, T_8, T_9, T_A, T_B, T_C, T_D, T_UnKnown }
+
 @JsonSerializable()
 class Course {
   late DateTime mainCreateTimeStamp;
